51495e91a6f91284678d85b9afc19f61d2efa5c9
BARATH G <47454425+bharath132@users.noreply.github.com>
2026-05-24 14:57:08 +0530

    Add warehouses API

---
+++ app/api/warehouses/route.ts
@@ -0,0 +1,8 @@
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+  const warehouses = await prisma.warehouse.findMany()
+
+  return NextResponse.json(warehouses)
+}
